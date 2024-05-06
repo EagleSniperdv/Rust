63ccff1b477a82048b66784c2e1048b050078329
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-05-06 19:46:58 +0300

    1

---
+++ rust-udemy/target/debug/deps/rust_udemy-9969c2fd65214fba.d
@@ -0,0 +1,5 @@
+C:\Users\User\OneDrive\Desktop\Projects\Rust\rust-udemy\target\debug\deps\librust_udemy-9969c2fd65214fba.rmeta: src\main.rs
+
+C:\Users\User\OneDrive\Desktop\Projects\Rust\rust-udemy\target\debug\deps\rust_udemy-9969c2fd65214fba.d: src\main.rs
+
+src\main.rs:
